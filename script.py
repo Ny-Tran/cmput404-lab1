2ea570f36f3eef1b318966bb94c7aa78224b14ce
Ny Tran <tntran1@ualberta.ca>
2021-01-11 19:26:50 -0700

    complete lab 1

--- script.py
+++ script.py
@@ -3,5 +3,7 @@ import requests
 print(requests.__version__)
 
 res = requests.get("http://www.google.com")
+print(res.status_code)
 
-print(res) 
+res = requests.get("https://raw.githubusercontent.com/Ny-Tran/cmput404-lab1/main/script.py")
+print(res.text) 
